2a67bd29cae0466f0d0ee82881f354f19be59129
tiscomsci29 <dd.stitch55@gmail.com>
2016-01-05 00:56:23 +0700

    commit test

---
+++ Test.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class Test {
+
+    /**
+     * Property Declaration
+     */
+    const MYCONST = 'const';  // ค่าคงที่
+    static $myStatic = 'static';
+    private $myPrivate = 'private';
+    protected $myProtected = 'protected';
+    public $myPublic = 'public';
+
+
+    /**
+     *  Method Declaration
+     */
+
+    // Constructor
+    function __construct() {
+        # self => this
+        self::MYCONST;
+        self::$myStatic;
+        self::getMyPrivate();
+    }
+
+
+    // Setter And Getter
+    public function setMyPrivete($pri = null){
+        $this->myPrivate = $pri;
+    }
+
+    public function getMyPrivate(){
+        return $this->myPrivate;
+    }
+
+
+    public function _toString(){
+
+    }
+
+}
+
+?>
